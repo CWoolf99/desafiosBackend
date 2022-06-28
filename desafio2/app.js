0a7fcec526e1b33535f31b27757e0fd22398688f
Christian Woolf <cwoolf210799@gmail.com>
2022-06-28 10:17:49 -0500

    correcciones desafio 2

--- desafio2/app.js
+++ desafio2/app.js
@@ -6,33 +6,40 @@ class Contenedor{
     }
 
 
-    save ( producto1 , productos ){ 
+   async save ( producto1 , productos ){ 
         let id = productos.length+1;
         console.log('el id es', id)
         let iD = {id:id}
         let productoF = {...iD, ...producto1} 
         productos.push(productoF);
-        guardar( this.archivo , productos )
+        await guardar( this.archivo , productos )
     };
 
     getById(id , productos ){
+        console.log(productos.find((valor)=> valor.id === id))
         return productos.find((valor)=> valor.id === id);
               
     };
 
-    getAll ( productos ) {
-        console.log( productos );
+    async getAll () {
+        try {
+
+            const contenido = await fs.promises.readFile(this.archivo, 'utf-8')
+            console.log(`se leyó el texto exitosamente` , JSON.parse(contenido))
+            return JSON.parse(contenido); 
+        }
+        catch (err) {
+            console.log('error en la lectura')
+        }
     };
 
-    deleteById ( id , productos ) {
-        productos = productos.filter(( valor )=> valor.id != id)
-        console.log(productos);
-        guardar( this.archivo , productos);
+   async deleteById ( id ) {
+        const productosTxt = await this.getAll();
+        guardar(this.archivo, productosTxt.filter(( valor )=> valor.id != id) );
     };
 
     deleteAll ( productos ) { 
         productos=[];
-        console.log(productos);
         guardar( this.archivo , productos);
     };
 
@@ -52,23 +59,6 @@ async function guardar ( archivo , producto) {
     }
 };
 
-
-/*async function leer ( archivo ) {
-    try {
-
-        const contenido = await fs.promises.readFile(archivo, 'utf-8')
-        console.log(`se leyó el texto exitosamente`)
-        let productos = contenido;
-        let productosL = JSON.parse(productos)
-        return productosL 
-
-    }
-    catch (err) {
-        
-    }
-};
-*/
-
 let product = new Contenedor('productos.txt')
 
 let producto1 = {nombre:'ab',precio:2,imagen:'a'};
@@ -83,14 +73,26 @@ let id = 1;
 
 product.save( producto1 , productos );
 
-product.save( producto2 , productos );
+setTimeout(() => {
+    product.save( producto2 , productos );  
+}, 3000);
 
-product.save( producto3 , productos );
+setTimeout(() => {
+    product.save( producto3 , productos );   
+}, 5000);
 
-product.getById(id , productos);
+setTimeout(() => {
+    product.getById(id , productos);  
+}, 6000);
 
-product.getAll(productos);
+setTimeout(() => {
+    product.getAll()    
+}, 7000);
 
-//product.deleteById( id , productos );
+setTimeout(() => {
+    product.deleteById( id );   
+}, 8000);
 
-//product.deleteAll( productos );
+setTimeout(() => {
+    product.deleteAll( productos );   
+}, 9000);
