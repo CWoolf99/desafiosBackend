a792fff79af5304eb257bcc71cc9f8440757e349
Christian Woolf <cwoolf210799@gmail.com>
2022-08-11 10:58:56 -0500

    solucion desafio 3

--- desafio3/desafio3.js
+++ desafio3/desafio3.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const fs = require('fs');
+
 const { get } = require('http');
 
 const app = express();
@@ -18,7 +20,7 @@ class Contenedor{
         let iD = {id:id}
         let productoF = {...iD, ...producto1} 
         productos.push(productoF);
-        await guardar( this.archivo , productos )
+        await this.guardar( this.archivo , productos )
     };
 
     getById(id , productos ){
@@ -31,26 +33,8 @@ class Contenedor{
         try {
 
             const contenido = await fs.promises.readFile(this.archivo, 'utf-8')
-            let productosD = JSON.parse(contenido);
-            let id =Math.floor( Math.random() * (3 - 1) + 1);
-            console.log(id)
-            let productoR = productosD.find((valor)=> valor.id === id);
-            console.log(`se leyó el texto exitosamente` , productosD , productoR )
-             app.get('/', ( req , res ) => {
-                res.send('desafio 3')
-            })
-            
-            app.get('/productos', ( req , res ) => {
-                res.send(productosD);
-            });
-            
-            app.get('/productoRandom', ( req , res ) => {
-                res.send(productoR)
-            });
-            
-            app.listen(port, () => {
-                console.log(`tu servidor está corriendo en el puerto ${port}`)
-            }); 
+            console.log(`se leyó el texto exitosamente` , JSON.parse(contenido))
+            return JSON.parse(contenido); 
         }
         catch (err) {
             console.log('error en la lectura')
@@ -59,59 +43,59 @@ class Contenedor{
 
    async deleteById ( id ) {
         const productosTxt = await this.getAll();
-        guardar(this.archivo, productosTxt.filter(( valor )=> valor.id != id) );
+        this.guardar(this.archivo, productosTxt.filter(( valor )=> valor.id != id) );
     };
 
     deleteAll ( productos ) { 
         productos=[];
-        guardar( this.archivo , productos);
+        this.guardar( this.archivo , productos);
     };
 
-    async displayProductos ( ) {
-       const  productosTxt = await this.getAll();
-       app.get('/', ( req , res ) => {
-            res.send('desafio 3')
-        })
-        
-        app.get('/productos', ( req , res ) => {
-            res.send(`${productosTxt}`);
-        });
-        
-        app.get('/productoRandom', ( req , res ) => {
-            res.send('producto random')
-        });
-        
-        app.listen(port, () => {
-            console.log(`tu servidor está corriendo en el puerto ${port}`)
-        });
+    async  guardar ( archivo , producto) {
+        let productoJ= JSON.stringify(producto)
+        console.log(productoJ)
+        try {
+    
+            await fs.promises.writeFile(archivo, `${productoJ}`)
+            console.log(`se agregó el texto exitosamente`)
+        }
+        catch (err) {
+            
+        }
     };
-
 }
 
 
 
-async function guardar ( archivo , producto) {
-    let productoJ= JSON.stringify(producto)
-    console.log(productoJ)
-    try {
-
-        await fs.promises.writeFile(archivo, `${productoJ}`)
-        console.log(`se agregó el texto exitosamente`)
-    }
-    catch (err) {
-        
-    }
-};
-
 let product = new Contenedor('productos.txt')
+let productosD=[];
 
-let producto1 = {nombre:'ab',precio:2,imagen:'a'};
+ app.get('/', ( req , res ) => {
+    res.send('desafio 3')
+    })
+            
+app.get('/productos', async( req , res ) => {
+    productosD = await product.getAll();
+    res.send(productosD);
+    });
+        
+app.get('/productoRandom', async ( req , res ) => {
+    productosD = await product.getAll();
+    let id =Math.floor( Math.random() * (3 - 1) + 1);
+    console.log(id)
+    let productoR = productosD.find((valor)=> valor.id === id);
+    console.log(productoR)
+    res.send(productoR)
+    });
+            
+app.listen(port, () => {
+    console.log(`tu servidor está corriendo en el puerto ${port}`)
+    }); 
 
-let producto2 = {nombre:'cd',precio:3,imagen:'b'};
-
-let producto3 = {nombre:'ef',precio:4,imagen:'c'};
 
 
-product.getAll();
+
+
+
 
 
